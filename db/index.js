1adaeaa4f4c76eec6121a6847e35c8b5d575fcf9
Ubuntu <ubuntu@ip-172-31-22-96.us-west-1.compute.internal>
2020-11-29 22:19:15 +0000

    .

--- db/index.js
+++ db/index.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 let db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
+  password: 'password',
   database: 'yelp_popular_dishes',
   multipleStatements: true
 });
